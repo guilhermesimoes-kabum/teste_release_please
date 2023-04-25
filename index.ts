963faacdbf5362c7a11f30984af6331b0ce47105
guilhermesimoes-kabum <guilherme.simoes@kabum.com.br>
2023-04-25 14:50:24 -0300

    feat: e o no bilhete ela dizia que iria me encontrar

--- index.ts
+++ index.ts
@@ -2,4 +2,5 @@ export function main() {
   console.log("hello world");
   console.log("cha la la la, cha la la in teh morning");
   console.log("e isso ai, um vendedor de flores");
+  console.log("E NO BILHETE ELA DIZIA QUE IRIA ME ENCONTRAR");
 }
